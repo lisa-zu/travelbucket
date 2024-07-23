eea3ef9df96ef2d9eddcc601e5d4ac3e5b275322
Roman <lisa.lz.zuchowski@gmail.com>
2024-07-23 17:08:20 +0200

    Initial Commit

---
+++ Travelbuckets/TravelbucketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelbucketsApp.swift
+//  Travelbuckets
+//
+//  Created by Roman Zuchowski on 23.07.24.
+//
+
+import SwiftUI
+
+@main
+struct TravelbucketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
